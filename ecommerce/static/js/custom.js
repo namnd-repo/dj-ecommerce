7525ee808747ac292711329bde6cd3bd191193e2
Nam <namnd.official@gmail.com>
2022-03-23 21:14:26 +0700

    add cart, checkout

--- ecommerce/static/js/custom.js
+++ ecommerce/static/js/custom.js
@@ -25,4 +25,66 @@ $(document).ready(function() {
             $(this).closest('.product_data').find('.qty-input').val(value);
         }
     });
+
+    $('.addToCartBtn').click(function (e) {
+        e.preventDefault();
+
+        var product_id = $(this).closest('.product_data').find('.prod_id').val();
+        var product_qty = $(this).closest('.product_data').find('.qty-input').val();
+        var token = $('input[name=csrfmiddlewaretoken]').val()
+        $.ajax({
+            method: "POST",
+            url: "/add-to-cart",
+            data: {
+                'product_id': product_id,
+                'product_qty': product_qty,
+                csrfmiddlewaretoken: token
+            },
+            success: function (response) {
+                console.log(response)
+                alertify.success(response.status)
+            }
+        })
+    });
+
+    $('.changeQuantity').click(function (e) {
+        e.preventDefault();
+
+        var product_id = $(this).closest('.product_data').find('.prod_id').val();
+        var product_qty = $(this).closest('.product_data').find('.qty-input').val();
+        var token = $('input[name=csrfmiddlewaretoken]').val()
+        $.ajax({
+            method: "POST",
+            url: "/update-cart",
+            data: {
+                'product_id': product_id,
+                'product_qty': product_qty,
+                csrfmiddlewaretoken: token
+            },
+            success: function (response) {
+                alertify.success(response.status)
+            }
+        })
+    });
+
+    $('.delete-cart-item').click(function (e) {
+        e.preventDefault();
+        var product_id = $(this).closest('.product_data').find('.prod_id').val();
+        var token = $('input[name=csrfmiddlewaretoken]').val()
+        
+        $.ajax({
+            method: "POST",
+            url: "delete-cart-item",
+            data: {
+                'product_id': product_id,
+                csrfmiddlewaretoken: token
+            },
+            success: function (response) {
+                alertify.success(response.status)
+                $('.cartdata').load(location.href + " .cartdata");
+            }
+        })
+
+    });
+
 }); 
--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path
 from . import views
-from store.controller import authview
+from store.controller import authview, cart, checkout
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('categories', views.categories, name='categories'),
     path('categories/<str:slug>', views.categoriesview, name='categoriesview' ),
     path('categories/<str:cate_slug>/<str:prod_slug>', views.productview, name='productview'),
+    
     path('register/', authview.register, name='register'),
-    path('login/', authview.loginpage, name='loginpage'),
-    path('logout/', authview.logoutpage, name='logout')
+    path('login/', authview.loginpage, name='loginpage'),   
+    path('logout/', authview.logoutpage, name='logout'),
+
+    path('add-to-cart', cart.addtocart, name='addtocart'),
+    path('cart', cart.viewcart, name='cart'),
+    path('update-cart', cart.updatecart, name='updatecart'),
+    path('delete-cart-item', cart.deletecartitem, name='deletecartitem'),
+
+    path('checkout', checkout.index, name="checkout"),
 ]
